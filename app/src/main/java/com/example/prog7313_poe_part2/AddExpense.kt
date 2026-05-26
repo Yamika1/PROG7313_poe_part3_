fb3a430f4da652d01ea6bc3fdd74abb2d46f30cf
Yamika1 <161420780+Yamika1@users.noreply.github.com>
2026-05-26 19:29:28 +0200

    commit 15

--- app/src/main/java/com/example/prog7313_poe_part2/AddExpense.kt
+++ app/src/main/java/com/example/prog7313_poe_part2/AddExpense.kt
@@ -29,6 +29,8 @@ class AddExpense : AppCompatActivity() {
 
     private var selectedCategory = "Groceries"
 
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_expense)
